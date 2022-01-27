c8a6230359b536637e71a74d0a4144523172952a
Michael Bisgaard Olesen <mo@concordium.com>
2022-01-27 20:16:43 +0100

    Replace codegen script with NPM project

--- codegen.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-set -eu
-
-npx @openapitools/openapi-generator-cli generate --input-spec=rosetta-specifications/api.yaml --generator-name=rust --output=rosetta --additional-properties=packageName=rosetta

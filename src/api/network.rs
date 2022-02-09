b07a5b538a519cb76d4110221949a600f48a263f
Michael Bisgaard Olesen <mo@concordium.com>
2022-02-09 14:22:11 +0100

    Network: Add missing fields

--- src/api/network.rs
+++ src/api/network.rs
@@ -2,6 +2,7 @@ use crate::api::network::ApiError::ClientRpcError;
 use concordium_rust_sdk::endpoints::{Client, RPCError};
 use rosetta::models::*;
 use serde::Serialize;
+use serde_json::json;
 use std::ops::Deref;
 use thiserror::Error;
 
@@ -65,7 +66,16 @@ impl NetworkApi {
                 middleware_version: Some(SERVER_VERSION.to_string()),
                 metadata: None,
             }),
-            allow: Box::new(Default::default()),
+            allow: Box::new(Allow {
+                operation_statuses: vec![],       // none yet
+                operation_types: vec![],          // none yet
+                errors: vec![], // TODO should be one result for each known error code?
+                historical_balance_lookup: false, // TODO do we support querying account balance at any block height?
+                timestamp_start_index: None, // not populated as the genesis block has a valid time stamp
+                call_methods: vec![],        // none yet
+                balance_exemptions: vec![], // TODO unsure what this is for - rewards are paid out without operations??
+                mempool_coins: false,       // mempool is not available
+            }),
         })
     }
 
@@ -82,19 +92,30 @@ impl NetworkApi {
             ));
         }
         let consensus_status = self.client.clone().get_consensus_status().await?;
+        let peer_list = self.client.clone().peer_list(false).await?;
         Ok(NetworkStatusResponse {
+            // Defining "current" block as last finalized block.
             current_block_identifier: Box::new(BlockIdentifier {
                 index: consensus_status.last_finalized_block_height.height as i64,
                 hash: consensus_status.last_finalized_block.to_string(),
             }),
-            current_block_timestamp: consensus_status.last_finalized_time.unwrap().timestamp(),
+            current_block_timestamp: consensus_status
+                .last_finalized_time
+                .map(|t| t.timestamp_millis())
+                .unwrap_or(-1),
             genesis_block_identifier: Box::new(BlockIdentifier {
                 index: 0,
                 hash: consensus_status.genesis_block.to_string(),
             }),
-            oldest_block_identifier: None,
-            sync_status: None,
-            peers: vec![],
+            oldest_block_identifier: None, // not relevant as the implementation doesn't prune blocks
+            sync_status: None, // the connected node's sync status is not easily available and thus currently not exposed here
+            peers: peer_list
+                .iter()
+                .map(|p| Peer {
+                    peer_id: p.node_id.to_string(),
+                    metadata: Some(json!({ "ip": p.ip, "port": p.port })),
+                })
+                .collect(),
         })
     }
 }

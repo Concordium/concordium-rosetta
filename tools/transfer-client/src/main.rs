eb2e359d428f8a6ef5f705a206c80347f58c5f96
Michael Bisgaard Olesen <mo@concordium.com>
2022-04-06 13:15:16 +0200

    Fold tool 'sign-transaction-hash' into 'transfer-client'

--- tools/transfer-client/src/main.rs
+++ tools/transfer-client/src/main.rs
@@ -1,33 +1,14 @@
 use anyhow::Result;
 use chrono::{Duration, Utc};
 use clap::AppSettings;
-use concordium_rust_sdk::common::types::{CredentialIndex, KeyIndex};
-use concordium_rust_sdk::id::types::AccountKeys;
 use concordium_rust_sdk::types::transactions::ExactSizeTransactionSigner;
-use concordium_rust_sdk::types::Memo;
 use reqwest::blocking::*;
 use reqwest::Url;
 use rosetta::models::*;
-use serde::{Deserialize, Serialize};
 use serde_json::value::Value;
-use sign_transaction_hash::{load_keys, sign_hash};
-use std::collections::BTreeMap;
-use std::convert::TryFrom;
 use std::ops::Add;
 use structopt::StructOpt;
-
-#[derive(Deserialize)]
-struct Metadata {
-    account_nonce: u64,
-}
-
-#[derive(Serialize)]
-struct Payload {
-    account_nonce: u64,
-    signature_count: u32,
-    expiry_unix_millis: u64,
-    memo: Option<Memo>,
-}
+use transfer_client::*;
 
 #[derive(StructOpt)]
 struct App {
@@ -63,8 +44,6 @@ struct App {
     )]
     memo_hex: Option<String>,
 }
-
-type SignatureMap = BTreeMap<CredentialIndex, BTreeMap<KeyIndex, Signature>>;
 
 fn main() -> Result<()> {
     // Parse CLI args.
@@ -174,25 +153,6 @@ fn main() -> Result<()> {
         submit_response.transaction_identifier.hash
     );
     Ok(())
-}
-
-fn parse_memo(memo_hex: Option<String>) -> Result<Option<Memo>> {
-    let memo = match memo_hex {
-        None => None,
-        Some(s) => {
-            let bs = hex::decode(s)?;
-            let m = Memo::try_from(bs)?;
-            Some(m)
-        }
-    };
-    Ok(memo)
-}
-
-fn signature_maps_to_signatures(signatures: Vec<SignatureMap>) -> Vec<Signature> {
-    signatures
-        .into_iter()
-        .flat_map(|s| s.values().flat_map(|x| x.values().cloned()).collect::<Vec<Signature>>())
-        .collect()
 }
 
 fn call_preprocess(
@@ -318,47 +278,6 @@ fn call_submit(
         .send()?
         .json()
         .map_err(reqwest::Error::into)
-}
-
-fn sign_payloads(payloads: Vec<SigningPayload>, keys: &AccountKeys) -> Result<Vec<SignatureMap>> {
-    payloads.iter().map(|p| sign_payload(p, keys)).collect()
-}
-
-fn sign_payload(payload: &SigningPayload, keys: &AccountKeys) -> Result<SignatureMap> {
-    let res = sign_hash(keys, payload.hex_bytes.as_str())?
-        .signatures
-        .iter()
-        .map(|(cred_idx, sigs)| {
-            (
-                *cred_idx,
-                sigs.iter()
-                    .map(|(key_idx, sig)| {
-                        let public_key = keys.keys[cred_idx].keys[key_idx].public;
-                        let public_key_hex = hex::encode(public_key);
-                        let sig_hex = format!(
-                            "{}:{}/{}",
-                            cred_idx.index,
-                            u8::from(*key_idx),
-                            hex::encode(sig)
-                        );
-                        (
-                            *key_idx,
-                            Signature {
-                                signing_payload: Box::new(payload.clone()),
-                                public_key: Box::new(PublicKey {
-                                    hex_bytes: public_key_hex,
-                                    curve_type: CurveType::Edwards25519,
-                                }),
-                                signature_type: SignatureType::Ed25519,
-                                hex_bytes: sig_hex,
-                            },
-                        )
-                    })
-                    .collect(),
-            )
-        })
-        .collect();
-    Ok(res)
 }
 
 fn test_transfer_operations(

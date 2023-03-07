36fe6597f1d8bb9cbb168ecb459d726331a6d1d3
rasmus-kirk <mail@rasmuskirk.com>
2023-03-07 15:05:01 +0100

    Ran rust formatter

--- src/api/network.rs
+++ src/api/network.rs
@@ -134,13 +134,15 @@ impl NetworkApi {
                                              * blocks */
             sync_status:              None, /* the connected node's sync status is not easily
                                              * available and thus currently not exposed here */
-            peers:                    Some(peer_list
-                .iter()
-                .map(|p| Peer {
-                    peer_id:  p.peer_id.0.clone(),
-                    metadata: Some(json!({ "ip": p.addr.ip(), "port": p.addr.port() })),
-                })
-                .collect()),
+            peers:                    Some(
+                peer_list
+                    .iter()
+                    .map(|p| Peer {
+                        peer_id:  p.peer_id.0.clone(),
+                        metadata: Some(json!({ "ip": p.addr.ip(), "port": p.addr.port() })),
+                    })
+                    .collect(),
+            ),
         })
     }
 }
